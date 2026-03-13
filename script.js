a9bfd560ffed407aa28f05b836a657186083ca3a
fabioflpereira <fabioflpereira1995@gmail.com>
2026-03-13 16:29:02 +0000

    responsividade dos sponsors e criada a pagina dos videos

--- script.js
+++ script.js
@@ -121,3 +121,20 @@ function openTab(tabId) {
   document.getElementById(tabId).classList.add("active");
   event.target.classList.add("active");
 }
+
+// Muda as tabs dos videos
+function openTab(tabId) {
+  const contents = document.querySelectorAll(".tab-content");
+  const buttons = document.querySelectorAll(".tab-button");
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  document.getElementById(tabId).classList.add("active");
+  event.target.classList.add("active");
+}
